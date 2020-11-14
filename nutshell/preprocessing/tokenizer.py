7c8ad76fe53da905d67325df64cd30041858faea
KrishnanSG <krishsg525@gmail.com>
2020-11-14 13:57:32 +0530

    Add Token class to handle tokens

--- nutshell/preprocessing/tokenizer.py
+++ nutshell/preprocessing/tokenizer.py
@@ -1,4 +1,36 @@
 from abc import ABC, abstractmethod
+from typing import List, Union
+
+
+class Token:
+    def __init__(self, raw_tokens: List[List[str]]):
+        self.__raw_tokens = raw_tokens
+
+    def get_sentence(self, sentence_id) -> List:
+        if 0 <= sentence_id <= len(self.__raw_tokens) - 1:
+            return self.__raw_tokens[sentence_id]
+        else:
+            raise Exception(f"Invalid sentence Id. Valid sentence ids are of range [0, {len(self.__raw_tokens) - 1}]")
+
+    def get_sentences(self):
+        for sentence in self.__raw_tokens:
+            yield sentence
+
+    def get_number_of_tokens(self, raw=True) -> Union[List, int]:
+        """
+        Returns the number of tokens per sentence
+        :param raw: If false returns the total number of tokens
+        :return: List of tokens per sentence (1*n) -> n number of sentences
+        """
+        count = list(map(len, self.get_sentences()))
+        return count if raw else sum(count)
+
+    def get_avg_token_per_sentence(self) -> float:
+        res = self.get_number_of_tokens()
+        return sum(res) / len(res)
+
+    def get_number_of_sentences(self):
+        return len(self.__raw_tokens)
 
 
 class BaseTokenizer(ABC):
@@ -8,10 +40,10 @@ class BaseTokenizer(ABC):
 
     @staticmethod
     @abstractmethod
-    def tokenize(corpus, *args):
+    def tokenize(corpus, *args) -> Token:
         pass
 
     @staticmethod
     @abstractmethod
-    def tokenize_into_sentences(corpus, *args):
+    def tokenize_into_sentences(corpus, *args) -> Token:
         pass
